f694e0ea198d53640c9e6cfddad51492bde54f12
ravikiran152005 <ravikiran152005@gmail.com>
2025-06-18 23:53:30 +0530

    Updated app.py with MySQL integration

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-FROM python:3.10-slim
+import os
+from flask import Flask
+from flask_mysqldb import MySQL      # For newer versions of flask-mysql 
+# from flask.ext.mysql import MySQL   # For older versions of flask-mysql
+app = Flask(__name__)
 
-RUN apt-get update && apt-get install -y \
-    default-libmysqlclient-dev gcc \
-    && apt-get clean
+mysql = MySQL()
 
-WORKDIR /app
-COPY . /app
+mysql_database_host = os.getenv("MYSQL_DATABASE_HOST", "localhost")
 
-RUN pip install --no-cache-dir -r requirements.txt
+# MySQL configurations
+app.config['MYSQL_USER'] = 'db_user'
+app.config['MYSQL_PASSWORD'] = 'Passw0rd'
+app.config['MYSQL_DB'] = 'employee_db'
+app.config['MYSQL_HOST'] = mysql_database_host
+mysql.init_app(app)
 
-EXPOSE 5000
-CMD ["python", "app.py"]
+@app.route("/")
+def main():
+    return "Welcome RAVIKIRAN!"
+
+@app.route('/how are you')
+def hello():
+    return 'I am good, how about you?'
+
+@app.route('/read from database')
+def read():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM employees")
+    result = [row[0] for row in cur.fetchall()]
+    cur.close()
+    return ",".join(result)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
